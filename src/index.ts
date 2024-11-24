2fa4f12be22e3d9c7d74d62261b5556b4b3f03b3
Gabriel Menoni <gabriel.menoni@catijr.com.br>
2024-11-24 06:44:32 -0300

    first routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-function ola(str: String) {
-    return str;
-}
+import express, { Request, Response } from 'express';
 
-console.log(ola("Bom dia!"));
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+//Rota Inicial
+app.get('/', (req: Request, res: Response) => {
+    res.send("Backend rodando com node + typescript");
+})
+
+app.post('/', (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    res.send(`O ${name} tem o Email: ${email}`);
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
